c4b279af95b487ecc143d99dd537a82ebc3394f3
Trevor Winn <35859863+ClumsyBuddy@users.noreply.github.com>
2022-10-06 10:24:18 -0400

    updated

--- ImageCompressor.py
+++ ImageCompressor.py
@@ -2,6 +2,8 @@ import os
 import sys
 from glob import glob
 from PIL import Image
+from time import sleep
+
 
 cols = 25
 lines = 10
@@ -12,6 +14,11 @@ path = "./"#sys.argv[1]
 
 basewidth = 0
 Resizename = ""
+
+MainDir = "ProcessedImages"
+WEBPDir = "WEBP"
+CompressedDir = "Compressed"
+
 print("Change Size? y or n")
 a = input()
 if a == 'y':
@@ -25,22 +32,33 @@ if int(quality) < 0 or int(quality) > 100:
     sys.exit(0)
 
 img_list = []
-for img_name in glob(path+'/*'):
+for img_name in glob("./"+MainDir+"/"+WEBPDir+'/*'):
     # one can use more image types(bmp,tiff,gif)
-    if img_name.endswith(".webp"):
+    if img_name.endswith(".webp"): #or img_name.endswith(".JPG") or img_name.endswith(".png"):
         # extract images name(image_name.[jpg|png]) from the full path
         img_list.append(img_name.split('\\')[-1])
 
 # print(img_list)   # for debug
 for img_name in img_list:
     name = img_name.split('.')
+    # sleep(5)
     if len(name) > 2:
         print("Periods cannot be in the name")
         sys.exit(0)
-    im = Image.open(path+img_name)
+    im = Image.open("./"+MainDir+"/"+WEBPDir+"/"+img_name)
+    size = basewidth, basewidth
+    new_im = Image.new('RGB', size, (255, 255, 255))
     if basewidth > 0:
-        wpercent = (basewidth/float(im.size[0]))
-        hsize = int((float(im.size[1])*float(wpercent)))
-        im = im.resize((basewidth, hsize), Image.ANTIALIAS)
+        #wpercent = (basewidth/float(im.size[0]))
+        #hsize = int((float(im.size[1])*float(wpercent)))
+        
+        im.thumbnail(size=size, resample=Image.Resampling.LANCZOS)
+        img_w, img_h = im.size
+        bg_w, bg_h = new_im.size
+        img_offset = ((bg_w - img_w) // 2, (bg_h - img_h) // 2)
+        new_im.paste(im, img_offset)
+        
+        #im = im.resize((basewidth, basewidth), Image.Resampling.LANCZOS)
         Resizename = f"-{basewidth}x"
-    im.save(name[0]+"-Compressed"+Resizename+"."+name[1], quality=quality, optimize=True)
+    #im = remove_transparency(im, (0,0,0))
+    new_im.save("./"+MainDir+"/"+CompressedDir+"/"+name[0]+"-Compressed"+Resizename+"."+name[1], quality=quality, optimize=True)
--- WEBP_Converter.py
+++ WEBP_Converter.py
@@ -5,11 +5,30 @@
 import sys
 from subprocess import call
 from glob import glob
+from PIL import Image
+from time import sleep
+import os
 
 #folder-name
 path = "./"#sys.argv[1]
 #quality of produced .webp images [0-100]
 quality = sys.argv[1]
+
+basewidth = 0
+
+MainDir = "ProcessedImages"
+JPGDir = "JPG"
+WEBPDir = "WEBP"
+CompressedDir = "Compressed"
+
+if not os.path.exists("./"+MainDir):
+    os.mkdir("./"+MainDir)
+if not os.path.exists("./"+MainDir+"/"+JPGDir):
+    os.mkdir("./"+MainDir+"/"+JPGDir)
+if not os.path.exists("./"+MainDir+"/"+WEBPDir):
+    os.mkdir("./"+MainDir+"/"+WEBPDir)
+if not os.path.exists("./"+MainDir+"/"+CompressedDir):
+    os.mkdir("./"+MainDir+"/"+CompressedDir)
 
 if int(quality) < 0 or int(quality) > 100:
     print("image quality out of range[0-100] ;/:/")
@@ -18,15 +37,45 @@ if int(quality) < 0 or int(quality) > 100:
 img_list = []
 for img_name in glob(path+'/*'):
     # one can use more image types(bmp,tiff,gif)
-    if img_name.endswith(".jpg") or img_name.endswith(".png") or img_name.endswith(".jpeg") or img_name.endswith(".PNG") or img_name.endswith(".jfif"):
+    if img_name.endswith(".jpg") or img_name.endswith(".png") or img_name.endswith(".jpeg") or img_name.endswith(".PNG") or img_name.endswith(".tif"):
         # extract images name(image_name.[jpg|png]) from the full path
         img_list.append(img_name.split('\\')[-1])
+        
 
+def remove_transparency(im, bg_colour=(255, 255, 255)):
+    # Only process if image has transparency
+    if im.mode in ('RGBA', 'LA') or (im.mode == 'P' and 'transparency' in im.info):
+        # Need to convert to RGBA if LA format due to a bug in PIL
+        alpha = im.convert('RGBA').getchannel("A")
+        # Create a new background image of our matt color.
+        # Must be RGBA because paste requires both images have the same format
+        bg = Image.new("RGBA", im.size, bg_colour + (255,))
+        bg.paste(im, mask=alpha)
+        new_im = bg.convert("RGB")
+        return new_im
+    else:
+        return im
 
 # print(img_list)   # for debug
 for img_name in img_list:
+    name = img_name.split('.')
+    print(path+img_name)
+    if  name[1] == "tif": #Basically tif files suck and we convert to jpg before converting to webp. 
+        im = Image.open(path+img_name)
+        im_rgb = remove_transparency(im=im)
+        im_rgb.save("./"+MainDir+"/"+JPGDir+"/"+name[0]+"."+"jpg")
+        cmd='cwebp \"'+path+'/'+MainDir+"/"+JPGDir+"/"+name[0]+".jpg"+'\" -q '+quality+'  -lossless -m 6 -mt -af -o \"'+path+'/'+MainDir+"/"+WEBPDir+"/"+(img_name.split('.')[0])+'.webp\"'
+        call(cmd, shell=False)
+    else:
+        cmd='cwebp \"'+path+'/'+img_name+'\" -q '+quality+'  -lossless -m 6 -mt -af -o \"'+path+"/"+MainDir+"/"+WEBPDir+'/'+(img_name.split('.')[0])+'.webp\"'
+        call(cmd, shell=False)
     # though the chances are very less but be very careful when modifying the below code
-    cmd='cwebp \"'+path+'/'+img_name+'\" -q '+quality+' -o \"'+path+'/'+(img_name.split('.')[0])+'.webp\"'
+    # cmd='cwebp \"'+path+'/'+name[0]+".jpg"+'\" -q '+quality+'  -lossless -m 6 -mt -o \"'+path+'/'+(img_name.split('.')[0])+'.webp\"'
+    #cmd='cwebp \"'+path+'/'+img_name+'\" -q '+quality+' -lossless -exact -m 6 -o \"'+path+'/'+(img_name.split('.')[0])+'.webp\"'
+    #cmd='cwebp \"'+path+'/'+img_name+'\" -q '+quality+' -o \"'+path+'/'+(img_name.split('.')[0])+'.webp\"'
     # running the above command
-    call(cmd, shell=False)
-    # print(cmd)    # for debug
+    #call(cmd, shell=False)
+
+cmd = "python ImageCompressor.py"
+call(cmd, shell=False)
+
